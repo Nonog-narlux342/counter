273f0e9a97bf875c3ba9cbf384d75b571765a378
Nonog-narlux342 <68525348+Nonog-narlux342@users.noreply.github.com>
2020-08-19 15:30:36 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="rounded-circle" src="./assets/1.jpg" width="20%" />
+    <HelloWorld msg="Nonog_Narlux" />
+    <hr />
+    <div class="row">
+      <div class="col">
+        <img class="rounded border border-dark" src="./assets/5.jpg" width="80%" />
+        <br />
+        <br />
+        <coun />
+      </div>
+      <div class="col">
+        <img class="rounded border border-dark" src="./assets/3.jpg" width="80%" />
+        <br />
+        <br />
+        <coun />
+      </div>
+      <div class="col">
+        <img class="rounded border border-dark" src="./assets/6.jpg" width="80%" />
+        <br />
+        <br />
+        <coun />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <fonts />
+        <hr />
+        <div class="row-12">
+          ชื่อ-นามสกุล
+          <input type="text" v-model="name" />
+          รหัส นศ.
+          <input type="number" v-model="code" />
+          GPA
+          <input type="number" v-model="gpa" step="0.1" />
+          <br />
+          <br />
+        </div>
+        <div class="row-12">แสดง:ชื่อ {{name}} รหัส นศ.{{code}} GPA {{gpa}}</div>
+        <br />
+        <br />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import coun from "./components/coun.vue";
+import fonts from "./components/fonts.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    coun,
+    fonts,
+  },
+  data: function () {
+    return {
+      total: 0,
+      name: "",
+      code: "",
+      gpa: 0.1,
+    };
+  },
+};
 </script>
 
 <style>
